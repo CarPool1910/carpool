02971ebb4fd62a4d656401607e872b6242d0c325
hkucheria007 <kucheriahrithik@gmail.com>
2020-11-06 13:27:34 +0530

    AboutActivity

--- app/src/main/java/com/dev/miniproject2/AboutActivity.java
+++ app/src/main/java/com/dev/miniproject2/AboutActivity.java
@@ -3,11 +3,13 @@ package com.dev.miniproject2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
     TextView textView;
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
